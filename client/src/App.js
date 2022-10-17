ece7662a9b8c4be3dd48b0ebe078911807949a47
aryanshah295 <aryan.shah.2953@gmail.com>
2022-10-17 23:28:48 +0530

    organized folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import SignIn from './components/SignIn/SignIn/SignIn';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
+      
     </div>
   );
 }
