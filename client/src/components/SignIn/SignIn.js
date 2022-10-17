e43eeba6cc28d8b11420bc75253ded46c1133558
aryanshah295 <aryan.shah.2953@gmail.com>
2022-10-18 01:11:03 +0530

    login and register pages done

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+import './SignIn.css';
 
 const SignIn = () => {
 
@@ -15,13 +17,34 @@ const SignIn = () => {
         setValues({...values, signInPassword : event.target.value});
     }
 
-    const onSubmitSignIn = () => {
-        
-    }
 
     return(
-        <div>
-            
+        <div className="SignIn">
+            <div className="container">
+                <h1>Login</h1>
+
+                <div className="user-info">
+                    <label for="email">Email : </label>
+                    <input 
+                    onChange={onEmailChange} 
+                    value={values.signInEmail} 
+                    type="text" 
+                    name="username" />
+                    <br/><br/>
+                    <label for="password">Password : </label>
+                    <input 
+                    onChange = {onPasswordChange}
+                    value = {values.signInPassword}
+                    type="password" 
+                    name="password" />
+                </div>
+                <div className="login-btn">
+                    <a href=""><h4>Login</h4></a>
+                </div>
+                <div className="sign-up">
+                    <h4>Don't have an account? <a href="">Sign up</a></h4>
+                </div>
+            </div>
         </div>
     );
 }
